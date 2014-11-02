cec6412cd42ea2e8ca85b90b101c3c8e49bd6514
Tatsuya Miyamae <miyamae@pixy.ne.jp>
2014-11-02 19:01:39 +0900

    Enabled assets pipeline live compile

--- config/application.rb
+++ config/application.rb
@@ -26,9 +26,5 @@ module TheApp
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
-    config.assets.precompile += %w(
-      admin.css admin.js
-    )
-
   end
 end
