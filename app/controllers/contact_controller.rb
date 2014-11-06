2bb38c7396a275c7ee3490dfea479048c19d5895
Tatsuya Miyamae <miyamae@pixy.ne.jp>
2014-11-07 02:55:31 +0900

    Fixed title when validation failed

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -5,17 +5,18 @@ class ContactController < ApplicationController
   add_breadcrumb 'お問い合わせ', :contact_index_path
 
   def index
-    @title = breadcrumbs.last.name
+    @title ||= breadcrumbs.last.name
     @contact ||= Contact.new
+    render :index
   end
 
   def create
-    @title = 'お問い合わせを受け付けました'
     @contact = Contact.new(params[:contact])
     if @contact.valid?
+      @title = 'お問い合わせを受け付けました'
       @contact.sendmail
     else
-      render :index
+      index
     end
   end
 
