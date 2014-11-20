2345041c268fc95fc68f5236a75fa2a269096169
Tatsuya Miyamae <miyamae@pixy.ne.jp>
2014-11-20 18:39:47 +0900

    Fixed failure in test

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,9 +2,9 @@ class ContactMailer < JpMailer
 
   def contact(contact)
     @contact = contact
-    mail to: ENV['email'],
+    mail to: ENV['email'] || 'dummy@example.com',
       subject: "[Contact] #{Const::PRODUCT}",
-      from: ENV['email']
+      from: ENV['email'] || 'no-reply@example.com'
   end
 
 end
