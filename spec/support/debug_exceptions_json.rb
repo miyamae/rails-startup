01cb561f88549e5f2b75ba88b58959066aaf7195
Tatsuya Miyamae <miyamae@pixy.ne.jp>
2014-11-28 01:50:46 +0900

    Disabled debug_exceptions_json for rspec

--- spec/support/debug_exceptions_json.rb
+++ spec/support/debug_exceptions_json.rb
@@ -1,6 +1,6 @@
-require 'debug_exceptions_json/rspec'
-
-RSpec.configure do |config|
-  config.include DebugExceptionsJson::RSpec::Hook
-  config.default_formatter = DebugExceptionsJson::RSpec::Formatter
-end
+# require 'debug_exceptions_json/rspec'
+#
+# RSpec.configure do |config|
+#   config.include DebugExceptionsJson::RSpec::Hook
+#   config.default_formatter = DebugExceptionsJson::RSpec::Formatter
+# end
