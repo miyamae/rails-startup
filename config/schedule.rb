dd8922a1279f0d9ba0b9882f538ca60fb18bf3c4
Tatsuya Miyamae <miyamae@pixy.ne.jp>
2014-11-30 01:52:16 +0900

    Added cron job for cleanup flushed cache

--- config/schedule.rb
+++ config/schedule.rb
@@ -26,3 +26,8 @@ set :environment, :production
 every 1.day, at: '4:00 am' do
   runner 'Session.sweep(updated: 5.hour, created: 5.days)'
 end
+
+# Cleanup flushed cache
+every 1.day, at: '3:00 am' do
+  command 'if [ -e /tmp/flushed_cache.* ]; then ionice -c 2 -n 7 nice -n 19 rm -rf /tmp/flushed_cache.*; fi'
+end
