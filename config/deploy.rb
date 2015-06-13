ccf700ff39fb2e024281db8da386d167ce7b85f6
Tatsuya Miyamae <miyamae@pixy.ne.jp>
2015-06-14 00:15:41 +0900

    Change deploy process

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, 'SampleApp'
 set :repo_url, 'https://github.com/miyamae/rails-startup.git'
 set :branch, 'master'
 set :rbenv_type, :system
-set :rbenv_ruby, '2.2.0'
+set :rbenv_ruby, '2.2.1'
 set :keep_releases, 5
 set :bundle_path, -> { shared_path.join('vendor/bundle') }
 set :linked_files, %w{ config/application.yml config/newrelic.yml }
@@ -11,15 +11,22 @@ set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 namespace :deploy do
 
-  desc 'Upload sample configuration files'
-  task :upload do
+  # init task
+  desc 'Upload configuration files. not overwrite'
+  task :upload_config do
     on roles(:app) do |host|
       if test("[ ! -d #{shared_path}/config ]")
         execute("mkdir -p #{shared_path}/config")
       end
-      if test("[ ! -e #{shared_path}/config/application.yml ]")
-        upload!('config/application.yml.sample', "#{shared_path}/config/application.yml")
+
+      if test("[ ! -e #{shared_path}/config/database.yml ]")
+        upload!('config/database.yml', "#{shared_path}/config/database.yml")
       end
+
+      if test("[ ! -e #{shared_path}/config/application.yml ]")
+        upload!('config/application.yml', "#{shared_path}/config/application.yml")
+      end
+
       if test("[ ! -e #{shared_path}/config/newrelic.yml ]")
         upload!('config/newrelic.yml.sample', "#{shared_path}/config/newrelic.yml")
       end
@@ -29,14 +36,15 @@ namespace :deploy do
   desc 'Create Database'
   task :db_create do
     on roles(:db) do |host|
-      within current_path do
-        with rails_env: fetch(:rails_env) do
+      with rails_env: fetch(:rails_env) do
+        within release_path do
           execute :bundle, :exec, :rake, 'db:create'
-        end
+      end
       end
     end
   end
 
+  # auto task
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
@@ -47,7 +55,7 @@ namespace :deploy do
     on roles(:app), in: :groups, limit: 3, wait: 10 do
       within release_path do
         with rails_env: :production do
-          execute :bundle, :exec, :rake, 'assets:precompile'
+          execute :rake, 'assets:precompile'
         end
       end
     end
@@ -56,14 +64,15 @@ namespace :deploy do
     on roles(:app), in: :groups, limit: 3, wait: 10 do
       within current_path do
         with rails_env: fetch(:rails_env) do
-          # execute :bundle, :exec, :rake, 'tmp:cache:flush'
-          execute :bundle, :exec, :rake, 'tmp:cache:clear'
+          execute :rake, 'tmp:cache:clear'
+          execute :rake, 'cache:clear'
         end
       end
     end
   end
 
-  before :starting, :upload
+  before :starting, :upload_config
+  before 'deploy:migrate', :db_create
   after :publishing, :assets_precompile
   after :assets_precompile, :restart
   after :restart, :clear_cache
