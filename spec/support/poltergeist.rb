d1350622e2b78ad82ae81885535973c96755a8ad
Tatsuya Miyamae <miyamae@pixy.ne.jp>
2015-06-14 00:16:42 +0900

    Add Capybara settings

--- spec/support/poltergeist.rb
+++ spec/support/poltergeist.rb
@@ -6,6 +6,12 @@ Capybara.register_driver :poltergeist do |app|
 end
 Capybara.default_driver = :poltergeist
 
+Capybara.configure do |config|
+  config.javascript_driver = :poltergeist
+  config.ignore_hidden_elements = true
+  config.default_wait_time = 5
+end
+
 # transactional fixtures for poltergeist
 class ActiveRecord::Base
   mattr_accessor :shared_connection
